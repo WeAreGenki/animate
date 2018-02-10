b5beb431c0000782480e1c5156e51a1e55b81b99
Max Milton <max@wearegenki.com>
2018-02-11 08:42:00 +1100

    Fix broken exports

--- src/index.js
+++
@@ -1,5 +0,0 @@
-// simply export the upstream package, all the magic happens at build time
-// export * from 'animateplus';
-
-// FIXME: Need to hack upstream source to support buble correctly
-export * from './animateplus';
