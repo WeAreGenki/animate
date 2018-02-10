4bec7a93a6f66eb7bdeb7ed6d5fa8d9cb2b808bb
Max Milton <max@wearegenki.com>
2018-02-11 08:16:18 +1100

    Add animateplus source locally because we need to hack it

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
 // simply export the upstream package, all the magic happens at build time
-export * from 'animateplus';
+// export * from 'animateplus';
+
+// FIXME: Need to hack upstream source to support buble correctly
+export * from './animateplus';
